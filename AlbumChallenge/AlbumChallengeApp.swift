33774c4b8e48e037780ea12e07e39a16cbc1f60d
Manuel Moyano <manuelm_98@hotmail.com>
2022-09-13 08:09:30 -0300

    Initial Commit

---
+++ AlbumChallenge/AlbumChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumChallengeApp.swift
+//  AlbumChallenge
+//
+//  Created by Manuel Moyano on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlbumChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
